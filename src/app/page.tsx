f3ffb63623465721d6648cfb00929997ef689e27
Ryan Yogan <ryanyogan@gmail.com>
2023-07-27 11:04:04 -0500

    wip // remove main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <div className="h-full">
-      <div className="flex justify-center items-center">
-        <Button>Hello</Button>
-      </div>
+    <div>
+      <div></div>
     </div>
   );
 }
