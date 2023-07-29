4830ab6cf494ec12a602001b157de09e0e8d4b28
Ryan Yogan <ryanyogan@gmail.com>
2023-07-29 00:17:19 -0500

    show the history

--- src/components/dashboard/recent.tsx
+++ src/components/dashboard/recent.tsx
@@ -1,3 +1,6 @@
+import { db } from "@/lib/db";
+import { getAuthSession } from "@/lib/next-auth";
+import { redirect } from "next/navigation";
 import {
   Card,
   CardContent,
@@ -5,17 +8,31 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import HistoryList from "./history-list";
 
-export default function Recent() {
+export default async function Recent() {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  const gameCount = await db.game.count({
+    where: {
+      userId: session.user.id,
+    },
+  });
+
   return (
     <Card className="col-span-4 lg:col-span-3">
       <CardHeader>
         <CardTitle className="font-bold text-2xl">Recent Activities</CardTitle>
-        <CardDescription>You have played a total of 7 games.</CardDescription>
+        <CardDescription>
+          You have played a total of {gameCount} games.
+        </CardDescription>
       </CardHeader>
 
       <CardContent className="max-h-[580px] overflow-scroll">
-        History
+        <HistoryList limit={10} userId={session.user.id} />
       </CardContent>
     </Card>
   );
